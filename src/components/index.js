d69f3c36cbdfb3fb6936d7135733ea3b9f4bb305
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-04 23:44:33 +0530

    addewd

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,22 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import Input from "./Input";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./PostCard";
+import PostForm from "./post-form/PostForm";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Input };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  RTE,
+  Signup,
+  Login,
+  Post,
+  PostForm,
+};
